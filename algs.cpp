a8877ec764c41c5acc58435c1f927818d86801a6
Alex Chen <83100991+alexchchen@users.noreply.github.com>
2024-03-04 15:25:18 -0700

    Add another UF implementation

--- algs.cpp
+++ algs.cpp
@@ -27,7 +27,60 @@ using namespace std;
 const double pi = 3.14159265358979323846;
 
 /**
- * Disjoint-set / union-find data struture
+ * Union-find disjoint set data struture
+ */
+class UF {
+private:
+    vector<int> parent;
+    vector<int> size;
+
+public:
+    UF(int n) {
+        parent.assign(n, 0);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        size.assign(n, 1);
+    }
+    
+    void make_set(int v) {
+        if (v < parent.size()) {
+            parent[v] = v;
+            size[v] = 1;
+        } else {
+            parent.push_back(v);
+            size.push_back(1);
+        }
+    }
+
+    int find_set(int v) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (size[a] < size[b])
+                swap(a, b);
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+
+    bool same_set(int a, int b) {
+        return find_set(a) == find_set(b);
+    }
+
+    int set_size(int v) {
+        return size[v];
+    }
+};
+
+/**
+ * Union-find disjoint set data struture
  */
 struct UF {
     vector<int> v;
